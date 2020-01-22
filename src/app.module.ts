9602f4291f4c3a20e2fb74c0cb5ddb5405fdbd16
Cole Henderson <cole@4gclinical.com>
2020-01-21 22:25:52 -0600

    messed up previous git, adding rest

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app/modules/components/app.component';
+import { NavBarComponent } from './app/core/header/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
