87e44a94435fd68af071286b10433d7b3e5587b5
Cole Henderson <cole@4gclinical.com>
2020-02-26 10:54:14 -0600

    created algorithm

--- lambdas/algorithms/algorithms.py
+++ lambdas/algorithms/algorithms.py
@@ -13,53 +13,104 @@ from boto3.dynamodb.conditions import Key
 dynamodb = boto3.resource('dynamodb')
 
 def calculate(event, context):
-    table = dynamodb.Table('BTC_day')
     index = event['iterator']['index'] + 1
     i = 0
 
-    minute_ttl = 86400
-    minute_gap = 60
-    
+    hour_ttl = 2628000
+    hour_gap = 3600
+
+    four_hour_ttl = 15768000
+    four_hour_gap = 14400
+
     day_ttl = 157680000
     day_gap = 86400
+
+    week_ttl = 0
+    week_gap = 604800
 
     while i < 1:
         if ((time.time() % 1) < 0.1):
             timestamp = int(time.time())
 
-            try:
-                # Query the table for the last 20 rows of data.
-                # TTL and gap values can easily be found in btc-populate.py
-                results = table.query(
-                    KeyConditionExpression = Key('s').eq('BTC') & Key('t').gt((timestamp + day_ttl) - (day_gap * 50))
-                )
-            except ClientError as e:
-                print(e.response['Error']['Code'])
-                print(e.response['ResponseMetadata']['HTTPStatusCode'])
-                print(e.response['Error']['Message'])
-            else:
-                if 'Items' in results:
-                    # Turn the returned object into a JSON string,
-                    # and pass it to pandas to make it readable for TA
-                    df = pd.read_json(json.dumps(results['Items']))
-
-                    rsi = ta.momentum.rsi(close = df["c"], n = 14, fillna = True)
-                    print(rsi)
-
-                    macd = ta.trend.macd(close = df["c"], n_fast = 12, n_slow = 26, fillna = True)
-                    print(macd)
-
-                    macd_diff = ta.trend.macd_diff(close = df["c"], n_fast = 12, n_slow = 26, n_sign = 9, fillna = True)
-                    print(macd_diff)
-
-                    macd_signal = ta.trend.macd_signal(close = df["c"], n_fast = 12, n_slow = 26, n_sign = 9, fillna = True)
-                    print(macd_signal)
-
-
-
+            # Get the different rsi and macd values based on
+            # the last 30 values from each table
+            get_values('BTC_hour', timestamp, hour_ttl, hour_gap, 30)
+            get_values('BTC_four_hour', timestamp, four_hour_ttl, four_hour_gap, 30)
+            get_values('BTC_day', timestamp, day_ttl, day_gap, 30)
+            get_values('BTC_week', timestamp, week_ttl, week_gap, 30)
 
             i += 1
 
     return {
         'index': index
     }
+
+def get_values(table, timestamp, ttl, gap, timeframe):
+    print(table)
+    dynamo_table = dynamodb.Table(table)
+    try:
+        # Query the table for the last `timeframe` rows of data.
+        # TTL and gap values can easily be found in btc-populate.py
+        results = dynamo_table.query(
+            KeyConditionExpression = Key('s').eq('BTC') & Key('t').gt((timestamp + ttl) - (gap * timeframe))
+        )
+    except ClientError as e:
+        print(e.response['Error']['Code'])
+        print(e.response['ResponseMetadata']['HTTPStatusCode'])
+        print(e.response['Error']['Message'])
+    else:
+        if 'Items' in results:
+            # Turn the returned object into a JSON string,
+            # and pass it to pandas to make it readable for TA
+            df = pd.read_json(json.dumps(results['Items']))
+
+            # Get the past `timeframe` rsi values in a dataframe
+            rsi = ta.momentum.rsi(close = df["c"], n = 14, fillna = True)
+            for i in range(0, timeframe):
+                if (rsi.iloc[i] < 100) and (rsi.iloc[i] > 0):
+                    # Find the first value in the list that
+                    # isn't equal to 100 or 0
+                    rsi_val = rsi.iloc[i]
+                    break
+
+            # Get the past `timeframe` MACD histogram values in a dataframe
+            macd_diff = ta.trend.macd_diff(close = df["c"], n_fast = 12, n_slow = 26, n_sign = 9, fillna = True)
+            # The very first one is always 0. Comparing these two
+            # next values allows us to see a reversal in trend
+            macd_diff_new = macd_diff.iloc[1]
+            macd_diff_prev = macd_diff.iloc[2]
+
+            if (table == 'BTC_hour'):
+                rsi_macd('hour', rsi_val, 20, 80, macd_diff_new, macd_diff_prev)
+            elif (table == 'BTC_four_hour'):
+                rsi_macd('four hour', rsi_val, 20, 80, macd_diff_new, macd_diff_prev)
+            elif (table == 'BTC_day'):
+                rsi_macd('day', rsi_val, 30, 70, macd_diff_new, macd_diff_prev)
+            elif (table == 'BTC_week'):
+                rsi_macd('week', rsi_val, 40, 60, macd_diff_new, macd_diff_prev)
+            else:
+                return
+
+def rsi_macd(timeframe, rsi, rsi_buy, rsi_sell, macd_diff_new, macd_diff_prev):
+    if (rsi < rsi_buy):
+        print('rsi buy')
+        # Going from negative to positive indicates bullish trend. Buy
+        if (macd_diff_new >= 0) and (macd_diff_prev < 0):
+            print('BUY BUY BUY: ', timeframe)
+            print('rsi: ', rsi)
+            print('macd new: ', macd_diff_new, ' macd prev: ', macd_diff_prev)
+        else:
+            return
+
+    elif (rsi > rsi_sell):
+        print('rsi sell')
+        # Going from positive to negative indicates bearish trend. Sell
+        if (macd_diff_new <= 0) and (macd_diff_prev > 0):
+            print('SELL SELL SELL: ', timeframe)
+            print('rsi: ', rsi)
+            print('macd new: ', macd_diff_new, ' macd prev: ', macd_diff_prev)
+        else:
+            return
+
+    else:
+        return
