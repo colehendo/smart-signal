470fd75fc61af1cf74c9643095c268e42fa09408
Cole Henderson <cole@4gclinical.com>
2020-02-26 16:25:12 -0600

    added bot and signal storage capabilities

--- lambdas/algorithms/algorithms.py
+++ lambdas/algorithms/algorithms.py
@@ -46,7 +46,6 @@ def calculate(event, context):
     }
 
 def get_values(table, timestamp, ttl, gap, timeframe):
-    print(table)
     dynamo_table = dynamodb.Table(table)
     try:
         # Query the table for the last `timeframe` rows of data.
@@ -81,21 +80,22 @@ def get_values(table, timestamp, ttl, gap, timeframe):
             macd_diff_prev = macd_diff.iloc[2]
 
             if (table == 'BTC_hour'):
-                rsi_macd('hour', rsi_val, 20, 80, macd_diff_new, macd_diff_prev)
+                rsi_macd(timestamp, 'hour', 'rsi_macd_hour', rsi_val, 20, 80, macd_diff_new, macd_diff_prev)
             elif (table == 'BTC_four_hour'):
-                rsi_macd('four hour', rsi_val, 20, 80, macd_diff_new, macd_diff_prev)
+                rsi_macd(timestamp, 'four hour', 'rsi_macd_four_hour', rsi_val, 20, 80, macd_diff_new, macd_diff_prev)
             elif (table == 'BTC_day'):
-                rsi_macd('day', rsi_val, 30, 70, macd_diff_new, macd_diff_prev)
+                rsi_macd(timestamp, 'day', 'rsi_macd_day', rsi_val, 30, 70, macd_diff_new, macd_diff_prev)
             elif (table == 'BTC_week'):
-                rsi_macd('week', rsi_val, 40, 60, macd_diff_new, macd_diff_prev)
+                rsi_macd(timestamp, 'week', 'rsi_macd_week', rsi_val, 40, 60, macd_diff_new, macd_diff_prev)
             else:
                 return
 
-def rsi_macd(timeframe, rsi, rsi_buy, rsi_sell, macd_diff_new, macd_diff_prev):
+def rsi_macd(timestamp, timeframe, bot, rsi, rsi_buy, rsi_sell, macd_diff_new, macd_diff_prev):
     if (rsi < rsi_buy):
-        print('rsi buy')
         # Going from negative to positive indicates bullish trend. Buy
         if (macd_diff_new >= 0) and (macd_diff_prev < 0):
+            strength = ((macd_diff_new - macd_diff_prev) / (100 - rsi))
+            send_signal(timestamp, timeframe, 'buy', strength, 'rsi_macd', bot)
             print('BUY BUY BUY: ', timeframe)
             print('rsi: ', rsi)
             print('macd new: ', macd_diff_new, ' macd prev: ', macd_diff_prev)
@@ -103,9 +103,10 @@ def rsi_macd(timeframe, rsi, rsi_buy, rsi_sell, macd_diff_new, macd_diff_prev):
             return
 
     elif (rsi > rsi_sell):
-        print('rsi sell')
         # Going from positive to negative indicates bearish trend. Sell
         if (macd_diff_new <= 0) and (macd_diff_prev > 0):
+            strength = (abs(macd_diff_new - macd_diff_prev) / rsi)
+            send_signal(timestamp, timeframe, 'sell', strength, 'rsi_macd', bot)
             print('SELL SELL SELL: ', timeframe)
             print('rsi: ', rsi)
             print('macd new: ', macd_diff_new, ' macd prev: ', macd_diff_prev)
@@ -114,3 +115,91 @@ def rsi_macd(timeframe, rsi, rsi_buy, rsi_sell, macd_diff_new, macd_diff_prev):
 
     else:
         return
+
+def send_signal(timestamp, timeframe, signal, strength, indicator, bot):
+    signals_table = dynamodb.Table('BTC_signals')
+    try:
+        # Query for the signals on a specific timeframe and a specific indicator
+        signals_result = signals_table.query(
+            KeyConditionExpression = Key('i').eq(indicator) & Key('tf').eq(timeframe)
+        )
+    except ClientError as e:
+        print(e.response['Error']['Code'])
+        print(e.response['ResponseMetadata']['HTTPStatusCode'])
+        print(e.response['Error']['Message'])
+    else:
+        # If this is the first signal on the specific timeframe and indicator
+        # or if this signal is different from the most recent signal
+        if ('Items' not in signals_result) or (signals_result['Items'][len(signals_result['Items']) - 1]['si'] != signal):
+            second_table = dynamodb.Table('BTC_second')
+            try:
+                # Query for the last 10 seconds of prices, just to be safe
+                second_result = second_table.query(
+                    KeyConditionExpression = Key('s').eq('BTC') & Key('t').gt((timestamp + 60) - 10)
+                )
+            except ClientError as e:
+                print(e.response['Error']['Code'])
+                print(e.response['ResponseMetadata']['HTTPStatusCode'])
+                print(e.response['Error']['Message'])
+            else:
+                if 'Items' in second_result:
+                    latest_price = second_result['Items'][len(second_result['Items']) - 1]['p']
+                    item = {
+                        'ts': timestamp,
+                        'tf': timeframe,
+                        'si': signal,
+                        'st': strength,
+                        'i': indicator,
+                        'p': latest_price
+                    }
+
+                    try:
+                        # Write in the latest signal
+                        signals_table.put_item(Item = item)
+                    except ClientError as e:
+                        print(e.response['Error']['Code'])
+                        print(e.response['ResponseMetadata']['HTTPStatusCode'])
+                        print(e.response['Error']['Message'])
+
+                    bots_table = dynamodb.Table('BTC_bots')
+                    try:
+                        # Get the row for the bot associated with the timeframe and indicator
+                        bots_result = bots_table.get_item(Key = {'name': bot})
+                    except ClientError as e:
+                        print(e.response['Error']['Code'])
+                        print(e.response['ResponseMetadata']['HTTPStatusCode'])
+                        print(e.response['Error']['Message'])
+                    else:
+                        if 'Item' in bots_result:
+                            if (signal == 'sell'):
+                                # The profit is the strength of the previous signal times the latest price.
+                                # This keeps the amount sold proportional to the last buy signal,
+                                # therefore emulating buying/selling a set amount of coins
+                                transaction = (signals_result['Items'][len(signals_result['Items']) - 1]['st'] * latest_price)
+                                balance = (bots_result['Item']['balance'] + transaction)
+                                success_rate = ((balance / 100000) * 100)
+
+                            else:
+                                # The cost of the amount bought is the strength of the signal times the latest price
+                                transaction = (strength * latest_price)
+                                balance = (bots_result['Item']['balance'] - transaction)
+                                success_rate = ((balance / 100000) * 100)
+
+
+                            try:
+                                # Update the associated bot with the latest values
+                                update_result = bots_table.update_item(
+                                    Key = {'name': bot},
+                                    UpdateExpression = "set balance = :b, success_rate = :s, indicator = :i, transaction_amt = :t",
+                                    ExpressionAttributeValues = {
+                                        ':b': balance,
+                                        ':s': success_rate,
+                                        ':i': indicator,
+                                        ':t': transaction
+                                    },
+                                    ReturnValues="UPDATED_NEW"
+                                )
+                            except ClientError as e:
+                                print(e.response['Error']['Code'])
+                                print(e.response['ResponseMetadata']['HTTPStatusCode'])
+                                print(e.response['Error']['Message'])
