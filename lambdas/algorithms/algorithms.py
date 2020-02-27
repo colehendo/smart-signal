1d278800adbe7c9c3f9719d9cc265d977e087ebb
Cole Henderson <cole@4gclinical.com>
2020-02-27 15:41:44 -0600

    quick fix

--- lambdas/algorithms/algorithms.py
+++ lambdas/algorithms/algorithms.py
@@ -80,24 +80,24 @@ def get_values(table, timestamp, ttl, gap, timeframe):
             macd_diff_prev = macd_diff.iloc[2]
 
             if (table == 'BTC_hour'):
-                rsi_macd(timestamp, 'hour', 'rsi_macd_hour', rsi_val, 20, 80, macd_diff_new, macd_diff_prev)
+                rsi_macd(timestamp, 'rsi_macd_hour', rsi_val, 20, 80, macd_diff_new, macd_diff_prev)
             elif (table == 'BTC_four_hour'):
-                rsi_macd(timestamp, 'four hour', 'rsi_macd_four_hour', rsi_val, 20, 80, macd_diff_new, macd_diff_prev)
+                rsi_macd(timestamp, 'rsi_macd_four_hour', rsi_val, 20, 80, macd_diff_new, macd_diff_prev)
             elif (table == 'BTC_day'):
-                rsi_macd(timestamp, 'day', 'rsi_macd_day', rsi_val, 30, 70, macd_diff_new, macd_diff_prev)
+                rsi_macd(timestamp, 'rsi_macd_day', rsi_val, 30, 70, macd_diff_new, macd_diff_prev)
             elif (table == 'BTC_week'):
-                rsi_macd(timestamp, 'week', 'rsi_macd_week', rsi_val, 40, 60, macd_diff_new, macd_diff_prev)
+                rsi_macd(timestamp, 'rsi_macd_week', rsi_val, 40, 60, macd_diff_new, macd_diff_prev)
             else:
                 return
 
-def rsi_macd(timestamp, timeframe, bot, rsi, rsi_buy, rsi_sell, macd_diff_new, macd_diff_prev):
+def rsi_macd(timestamp, bot, rsi, rsi_buy, rsi_sell, macd_diff_new, macd_diff_prev):
     if (rsi < rsi_buy):
         # Going from negative to positive indicates bullish trend. Buy
         if (macd_diff_new >= 0) and (macd_diff_prev < 0):
             strength = round(Decimal(((macd_diff_new - macd_diff_prev) / (100 - rsi))), 10)
-            check_signal(timestamp, timeframe, 'buy', strength, 'rsi_macd', bot)
-            check_signal(timestamp, 'all', 'buy', strength, 'rsi_macd', 'rsi_macd_all')
-            print('BUY BUY BUY: ', timeframe)
+            check_signal(timestamp, 'buy', strength, 'rsi_macd', bot)
+            check_signal(timestamp, 'buy', strength, 'rsi_macd', 'rsi_macd_all')
+            print('BUY BUY BUY: ', __BuiltinFunction__)
             print('rsi: ', rsi)
             print('macd new: ', macd_diff_new, ' macd prev: ', macd_diff_prev)
         else:
@@ -107,9 +107,9 @@ def rsi_macd(timestamp, timeframe, bot, rsi, rsi_buy, rsi_sell, macd_diff_new, m
         # Going from positive to negative indicates bearish trend. Sell
         if (macd_diff_new <= 0) and (macd_diff_prev > 0):
             strength = round(Decimal((abs(macd_diff_new - macd_diff_prev) / rsi)), 10)
-            check_signal(timestamp, timeframe, 'sell', strength, 'rsi_macd', bot)
-            check_signal(timestamp, 'all', 'sell', strength, 'rsi_macd', 'rsi_macd_all')
-            print('SELL SELL SELL: ', timeframe)
+            check_signal(timestamp, 'sell', strength, 'rsi_macd', bot)
+            check_signal(timestamp, 'sell', strength, 'rsi_macd', 'rsi_macd_all')
+            print('SELL SELL SELL: ', bot)
             print('rsi: ', rsi)
             print('macd new: ', macd_diff_new, ' macd prev: ', macd_diff_prev)
         else:
@@ -118,13 +118,12 @@ def rsi_macd(timestamp, timeframe, bot, rsi, rsi_buy, rsi_sell, macd_diff_new, m
     else:
         return
 
-def check_signal(timestamp, timeframe, signal, strength, indicator, bot):
+def check_signal(timestamp, signal, strength, indicator, bot):
     table = dynamodb.Table('BTC_signals')
     try:
         # Query for the signals on a specific timeframe and a specific indicator
         signals_result = table.query(
-            KeyConditionExpression = Key('i').eq(indicator),
-            FilterExpression = Key('tf').eq(timeframe)
+            KeyConditionExpression = Key('i').eq(bot) & Key('ts').gt(0)
         )
     except ClientError as e:
         print(e.response['Error']['Code'])
@@ -135,17 +134,17 @@ def check_signal(timestamp, timeframe, signal, strength, indicator, bot):
         # or if this signal is different from the most recent signal
         if ('Items' not in signals_result) or (signals_result['Count'] == 0):
             if (signal == 'buy'):
-                write_signal(timestamp, timeframe, signal, strength, indicator, bot, 0)
+                write_signal(timestamp, signal, strength, indicator, bot, 0)
             else:
                 return
 
         elif (signals_result['Items'][len(signals_result['Items']) - 1]['si'] != signal):
-            write_signal(timestamp, timeframe, signal, strength, indicator, bot, signals_result['Items'][len(signals_result['Items']) - 1]['st'])
+            write_signal(timestamp, signal, strength, indicator, bot, signals_result['Items'][len(signals_result['Items']) - 1]['st'])
         
         else:
             return
 
-def write_signal(timestamp, timeframe, signal, strength, indicator, bot, original_strength):
+def write_signal(timestamp, signal, strength, indicator, bot, original_strength):
     table = dynamodb.Table('BTC_second')
     try:
         # Query for the last 10 seconds of prices, just to be safe
@@ -161,10 +160,9 @@ def write_signal(timestamp, timeframe, signal, strength, indicator, bot, origina
             latest_price = second_result['Items'][len(second_result['Items']) - 1]['p']
             item = {
                 'ts': timestamp,
-                'tf': timeframe,
                 'si': signal,
                 'st': strength,
-                'i': indicator,
+                'i': bot,
                 'p': latest_price
             }
 
