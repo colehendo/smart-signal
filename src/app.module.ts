82ca64d2540948aeef721140d5b0783008f56e80
Cole Henderson <cole@4gclinical.com>
2020-02-28 11:50:28 -0600

    cleaned up other packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ChartModule } from 'angular2-highcharts';
 import { HighchartsChartModule } from 'highcharts-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './app/core/header/header.component';
-
-import { FusionChartsModule } from 'angular-fusioncharts';
-
-import * as FusionCharts from 'fusioncharts';
-import * as Charts from 'fusioncharts/fusioncharts.charts';
-import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
-
-// Pass the fusioncharts library and chart modules
-FusionChartsModule.fcRoot(FusionCharts, Charts, FusionTheme);
 
 import { AppComponent } from './app/components/app.component';
 import { HomeComponent } from './app/components/home/home.component';
@@ -42,9 +32,7 @@ import { GraphComponent } from './app/shared/components/graph/graph.component'; 
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
-    HighchartsChartModule,
-    // ChartModule.forRoot(require('highcharts')),
-    FusionChartsModule
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
