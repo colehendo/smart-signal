1d5f8cf66abd3285d5440497d178f15f163a9a4f
Ryan Joseph <ryanjosephjr97@gmail.com>
2020-03-04 21:48:23 -0600

    change stuff with graph

--- src/app/shared/components/graph/graph.component.ts
+++ src/app/shared/components/graph/graph.component.ts
@@ -4,7 +4,7 @@ import { btc_week } from '../../modules/btc_week';
 import { btc_month } from '../../modules/btc_month';
 
 import * as _ from 'lodash';
-import * as Highcharts from 'highcharts';
+import * as Highcharts from 'highcharts/highstock';
 import HighchartsMore from 'highcharts/highcharts-more';
 HighchartsMore(Highcharts);
 
@@ -20,21 +20,30 @@ export class GraphComponent implements OnInit {
 
   constructor(private appRef: ApplicationRef) { }
 
+  public chartOptions: Highcharts.Options = {
+    series: [{
+      data: [
+        [600, 702,795, 849, 971],
+        [760, 801, 848, 895, 965],
+        [733, 853, 939, 980, 1080],
+        [714, 762, 817, 870, 918],
+        [724, 802, 806, 871, 950],
+        [834, 836, 864, 882, 910],
+      ],
+      type: 'boxplot'
+    }],
+  };
+
   public Highcharts: typeof Highcharts = Highcharts;
   public socket;
   public candles: any;
   public updateFlag: boolean = false;
 
-  public chartOptions: Highcharts.Options = {
-    series: [{
-      data: [],
-      type: 'boxplot'
-    }],
-  };
+
 
   ngOnInit() { this.setupWebSocket() }
 
-  async setupWebSocket() {
+  setupWebSocket() {
     
     this.socket = new ReconnectingWebSocket("wss://rix9fti73l.execute-api.us-east-1.amazonaws.com/prod");
 
@@ -46,12 +55,15 @@ export class GraphComponent implements OnInit {
 
     this.socket.onmessage = (message) => {
       this.candles = JSON.parse(message.data).prices;
-      console.log(this.candles)
-      // _.forEach(this.candles, (item) => {
-      //   this.chartOptions.series[0]['data'].push([item.h, item.l, item.o, item.c])
-      // });
-      this.chartOptions.series[0]['data'].push(10, 1, 4, 5)
-      this.updateFlag = true;
+      console.log(this.candles) //Got up to this point for sure
+      _.forEach(this.candles, (item) => {
+        this.chartOptions.series[0]['data'].push([item.h, item.l, item.o, item.c])
+      });
+      // this.chartOptions.series[0]['data']=[];
+      // this.chartOptions.series[0]['data'].push([10, 1, 4, 5,7])
+      console.log("This message should be followed by a the list of data in the series")
+      console.log(this.chartOptions.series[0]['data'])
+      // this.updateFlag = true;
       this.appRef.tick();
     };
   }
