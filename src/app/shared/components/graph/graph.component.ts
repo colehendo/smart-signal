3cfd3b6d806d41a2c650ce088c2be0a87340226d
Cole Henderson <cole@4gclinical.com>
2020-03-04 19:54:57 -0600

    data loaded, no refresh

--- src/app/shared/components/graph/graph.component.ts
+++ src/app/shared/components/graph/graph.component.ts
@@ -3,6 +3,7 @@ import ReconnectingWebSocket from 'reconnecting-websocket';
 import { btc_week } from '../../modules/btc_week';
 import { btc_month } from '../../modules/btc_month';
 
+import * as _ from 'lodash';
 import * as Highcharts from 'highcharts';
 import HighchartsMore from 'highcharts/highcharts-more';
 HighchartsMore(Highcharts);
@@ -16,64 +17,41 @@ HighchartsMore(Highcharts);
 
 
 export class GraphComponent implements OnInit {
+
+  constructor() { }
+
   public Highcharts: typeof Highcharts = Highcharts;
   public socket;
   public candles: any;
+  public dataReady = false;
+
+  public chartOptions: Highcharts.Options = {
+    series: [{
+      data: [],
+      type: 'boxplot'
+    }],
+  };
 
   ngOnInit() { this.setupWebSocket() }
 
-  setupWebSocket() {
+  async setupWebSocket() {
+    
     this.socket = new ReconnectingWebSocket("wss://rix9fti73l.execute-api.us-east-1.amazonaws.com/prod");
-    console.log(this.socket)
 
     this.socket.onopen = (event) => {
-      console.log(event)
       let data = {"action": "getPrices"};
       // this.socket.send(data);
       this.socket.send(JSON.stringify(data));
     };
 
-    this.socket.onmessage = function(message) {
-      console.log(message)
-      this.candles = JSON.parse(message.data);
+    this.socket.onmessage = (message) => {
+      this.candles = JSON.parse(message.data).prices;
       console.log(this.candles)
+      let newData = [];
+      _.forEach(this.candles, (item) => {
+        this.chartOptions.series[0]['data'].push([item.h, item.l, item.o, item.c])
+      });
     };
   }
-    public chartOptions: Highcharts.Options = {
-      series: [{
-        data: [
-          [760, 801, 848, 895, 965],
-          [733, 853, 939, 980, 1080],
-          [714, 762, 817, 870, 918],
-          [724, 802, 806, 871, 950],
-          [834, 836, 864, 882, 910],
-          [600, 702,795, 849, 971],
-          [760, 801, 848, 895, 965],
-          [733, 853, 939, 980, 1080],
-          [714, 762, 817, 870, 918],
-          [724, 802, 806, 871, 950],
-          [834, 836, 864, 882, 910],
-          [600, 702,795, 849, 971],
-          [760, 801, 848, 895, 965],
-          [733, 853, 939, 980, 1080],
-          [714, 762, 817, 870, 918],
-          [724, 802, 806, 871, 950],
-          [834, 836, 864, 882, 910],
-          [600, 702,795, 849, 971],
-          [760, 801, 848, 895, 965],
-          [799, 953, 1039, 980, 1080],
-          [714, 762, 817, 870, 918],
-          [724, 802, 806, 871, 950],
-          [834, 836, 864, 882, 910],
-          [600, 702,795, 849, 971],
-        ],
-        type: 'boxplot'
-      }]
-    };
-
-  constructor(
-  ) { }
-
-  
 
 }
