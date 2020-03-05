9f5930fd9ab39e32bb829800cd4d28b74f1f003b
Ryan Joseph <ryanjosephjr97@gmail.com>
2020-03-04 19:12:18 -0600

    some more shit

--- src/app/shared/components/graph/graph.component.ts
+++ src/app/shared/components/graph/graph.component.ts
@@ -17,6 +17,28 @@ HighchartsMore(Highcharts);
 
 export class GraphComponent implements OnInit {
   public Highcharts: typeof Highcharts = Highcharts;
+  public socket;
+  public candles: any;
+
+  ngOnInit() { this.setupWebSocket() }
+
+  setupWebSocket() {
+    this.socket = new ReconnectingWebSocket("wss://rix9fti73l.execute-api.us-east-1.amazonaws.com/prod");
+    console.log(this.socket)
+
+    this.socket.onopen = (event) => {
+      console.log(event)
+      let data = {"action": "getPrices"};
+      // this.socket.send(data);
+      this.socket.send(JSON.stringify(data));
+    };
+
+    this.socket.onmessage = function(message) {
+      console.log(message)
+      this.candles = JSON.parse(message.data);
+      console.log(this.candles)
+    };
+  }
     public chartOptions: Highcharts.Options = {
       series: [{
         data: [
@@ -52,45 +74,6 @@ export class GraphComponent implements OnInit {
   constructor(
   ) { }
 
-  public socket;
-  public candles: any;
-
-  ngOnInit() { this.setupWebSocket() }
-
-  setupWebSocket() {
-    this.socket = new ReconnectingWebSocket("wss://rix9fti73l.execute-api.us-east-1.amazonaws.com/prod");
-    console.log(this.socket)
-
-    this.socket.onopen = (event) => {
-      console.log(event)
-      let data = {"action": "getPrices"};
-      // this.socket.send(data);
-      this.socket.send(JSON.stringify(data));
-    };
-
-<<<<<<< HEAD
-  // testFunction() {
-  //   this.graph_data = this.btc_month;
-  //   console.log(this.graph_data[0])
-  // }
-
-  }
-}
-
-
-
-	// //Data will take in five parameters; [time, open, close, high ,d]
-	// title="Bitcoin";
-	// type="CandlestickChart";
-	// data=[];
-	// columnNames=['time','a','b','c','d'];
-=======
-    this.socket.onmessage = function(message) {
-      console.log(message)
-      this.candles = JSON.parse(message.data);
-      console.log(this.candles)
-    };
-  }
+  
 
 }
->>>>>>> 441b9f0de5db8167b3ce69e1f1f4eb04617bd34c
