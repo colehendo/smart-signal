5981248dbd67ecb98550bf4fe0586a48128e2ceb
Cole Henderson <cole@4gclinical.com>
2020-04-17 17:04:39 -0500

    updated script

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ aws s3 sync --delete ./dist/smart-signal s3://smart-signal --profile smart-signa
 echo ""
 echo "Updating files on EC2..."
 echo ""
-ssh -i ./smart-signal-frontend.pem -tt ec2-user@3.226.158.116 "sudo aws s3 sync --delete s3://smart-signal/ /var/www/html/"
+ssh -i ./smart-signal-frontend.pem -tt ec2-user@3.226.158.116 "sudo aws s3 sync s3://smart-signal/ /var/www/html/"
 
 echo ""
 echo "Site has been updated."
