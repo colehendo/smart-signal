30f3852a15e87dc480f8b3e9cf69aee376505ad1
unknown <you@example.com>
2020-04-26 16:46:01 -0500

    psar

--- lambdas/src/indicators/indicators.py
+++ lambdas/src/indicators/indicators.py
@@ -431,22 +431,39 @@ def mi(params, candles):
 
 # Parabolic Stop And Reverse
 # we'll trigger signals whenever we have a reversal so if
-def psar(params, candles):
+def psar(params, candles, timeframe):
     all_psar = ta.trend.PSARIndicator(high = candles["h"], low = candles["l"], close = candles["c"], step = 0.02, max_step = 0.2, fillna = False)
     print('Parabolic Stop And Reverse')
     psar = all_psar.psar()
     # print('psar: ', psar)
     psar_signals = []
     trend = "flat"
+    last_signal = "hold"
     ##make sure to account for last signal
     for i in range(len(psar)):
         current_psar = psar.iloc[i]
         curr_price = candles["c"][i];
-        if trend == "up" and current_psar > curr_price:
-            psar_signals.append({'sig': 'sell', 'str': 100})
+        if trend == "up" and current_psar > curr_price and last_signal != 'sell':
+            last_signal = 'sell'
+            psar_signals.append({
+            'indicator': 'psar',
+            'sig': 'sell',
+            'price': float(candles['c'][i]),
+            'time': int(candles['t'][i]),
+            'tf': timeframe,
+            'str': 100
+            })
             trend = "down"
-        elif trend == "down" and current_psar < curr_price:
-            psar_signals.append({'sig': 'buy', 'str': 100})
+        elif trend == "down" and current_psar < curr_price and last_signal != 'buy':
+            last_signal = 'buy'
+            psar_signals.append({
+            'indicator': 'psar',
+            'sig': 'buy',
+            'price': float(candles['c'][i]),
+            'time': int(candles['t'][i]),
+            'tf': timeframe,
+            'str': 100
+            })
             trend = "up"
         else:
             if current_psar > curr_price:
@@ -455,6 +472,7 @@ def psar(params, candles):
                 trend = "up"
             else:
                 trend = "flat"
+
     for i in range(0, len(psar_signals)):
         print(psar_signals[i][0])
 
