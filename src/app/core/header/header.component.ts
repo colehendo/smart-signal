f75caa8a091dc1b409b9f10ad01514a9c45bd572
Cole Henderson <cole@4gclinical.com>
2020-04-27 19:28:09 -0500

    the shits

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, ApplicationRef } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,18 +6,49 @@ import { Router } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements DoCheck {
+
+  public isLoggedIn = false;
 
   constructor(
     private router: Router,
+    private appRef: ApplicationRef
   ) { }
 
-  ngOnInit() {
+  public loginUrl = "https://smartsignal.auth.us-east-1.amazoncognito.com/login?client_id=62oatdg8jhsreqbobds4hp9omr&response_type=code&scope=email+openid&redirect_uri=https://www.smartsignal.watch/redirect"
+  public signupUrl = "https://smartsignal.auth.us-east-1.amazoncognito.com/signup?client_id=62oatdg8jhsreqbobds4hp9omr&response_type=code&scope=email+openid&redirect_uri=https://www.smartsignal.watch/redirect"
+
+  ngDoCheck() {
+    console.log('doing the thing')
+    if (localStorage.getItem('authCode')) {
+      this.isLoggedIn = true;
+    }
+    if ((window.location.href).includes("localhost")) {
+      this.loginUrl = "https://smartsignal.auth.us-east-1.amazoncognito.com/login?client_id=62oatdg8jhsreqbobds4hp9omr&response_type=code&scope=email+openid&redirect_uri=http://localhost:4200/redirect"
+      this.signupUrl = "https://smartsignal.auth.us-east-1.amazoncognito.com/signup?client_id=62oatdg8jhsreqbobds4hp9omr&response_type=code&scope=email+openid&redirect_uri=http://localhost:4200/redirect"
+    }
+  }
+
+  home() {
+    if (this.isLoggedIn) {
+      this.router.navigate(['/home']);
+    }
+    else {
+      this.router.navigate(['/']);
+    }
+  }
+
+  login() {
+    window.location.href = this.loginUrl
+  }
+
+  signup() {
+    window.location.href = this.signupUrl
   }
 
   logout() {
     localStorage.clear();
-    this.router.navigate(['/home']);
+    this.router.navigate(['/']);
   }
 
 }
