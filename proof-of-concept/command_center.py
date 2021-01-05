cdd0b158b67e64294acbc1149bf18ab3fa0f9dad
Cole Henderson <cole@4gclinical.com>
2021-01-04 23:51:59 -0500

    fixed visuals

--- proof-of-concept/command_center.py
+++ proof-of-concept/command_center.py
@@ -1,14 +1,101 @@
-from find_max_profit import FindMaxProfit
-from shared.data import Visuals
+from argparse import ArgumentParser
 import os
 
+from find_max_profit import FindMaxProfit
+from shared.data import Info, Visuals
+
+
+def get_argument_choices(
+    all_assets: [dict], asset_types: [str], timeframes: [dict]
+) -> ([str], [str]):
+    timeframe_options = list(timeframes.keys())
+    symbol_options = [
+        symbol for asset_name in asset_types for symbol in all_assets[asset_name]
+    ]
+
+    return sorted(symbol_options), sorted(timeframe_options)
+
+
+def parse_arguments(all_assets: [dict], asset_types: [str], timeframes: [dict]):
+    symbol_options, timeframe_options = get_argument_choices(
+        all_assets=all_assets, asset_types=asset_types, timeframes=timeframes
+    )
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--symbol",
+        "-s",
+        choices=symbol_options,
+        dest="symbol",
+        help="Symbol of the asset to run the algorithm on. AAPL, BTC, USD, etc.",
+        required=True,
+    )
+    parser.add_argument(
+        "--timeframe",
+        "-tf",
+        choices=timeframe_options,
+        dest="timeframe",
+        help="Period of time per candle.",
+        required=True,
+    )
+    parser.add_argument(
+        "--time_gap",
+        "-tg",
+        dest="time_gap",
+        help="INT. Max number of candles to hold a position for the max profit algorithm.",
+        type=int,
+    )
+
+    parser.add_argument(
+        "--display_data",
+        "-dd",
+        default=True,
+        dest="display_data",
+        help="BOOL. Whether to display the data in a line graph or not. Default is True.",
+        type=bool,
+    )
+    return parser.parse_args()
+
+
+def get_asset_type(all_assets: [dict], asset_types: [str], symbol: str) -> str:
+    key_list = list(all_assets.keys())
+    for key in key_list:
+        if symbol in all_assets[key]:
+            return key
+
+    raise ValueError(f"{symbol} does not belong to an asset type")
+
+
 def main():
-    asset_type = "cryptocurrency"
-    symbol = "BTC"
-    timeframe="hour"
-    max_profit = FindMaxProfit(asset_type=asset_type, symbol=symbol, timeframe=timeframe).max_profit()
-    print(max_profit)
-    Visuals().plot()
+    info_class = Info()
+    assets = info_class.get_asset_data()
+    timeframes = info_class.get_timeframe_data()
+    asset_types = list(assets.keys())
+
+    args = parse_arguments(
+        all_assets=assets, asset_types=asset_types, timeframes=timeframes
+    )
+    symbol = args.symbol
+    timeframe = args.timeframe
+    time_gap = args.time_gap
+    if not time_gap:
+        time_gap = int(timeframes[timeframe].get("time-gap"))
+
+    asset_type = get_asset_type(
+        all_assets=assets, asset_types=asset_types, symbol=symbol
+    )
+
+    max_profit = FindMaxProfit().max_profit(
+        asset_type=asset_type, symbol=symbol, timeframe=timeframe, time_gap=time_gap
+    )
+
+    if args.display_data:
+        Visuals().plot(
+            asset_type=asset_type,
+            symbol=symbol,
+            timeframe=timeframe,
+            transactions=max_profit,
+        )
 
 
 if __name__ == "__main__":
--- proof-of-concept/shared/data.py
+++ proof-of-concept/shared/data.py
@@ -1,7 +1,8 @@
 import json
+import os
+
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
 
 
 class Info:
@@ -13,38 +14,36 @@ class Info:
     def get_asset_data(self):
         filename = os.path.join(self._dirname, self._assets_relpath)
         with open(filename) as a:
-            assets = json.load(a)
-        return assets
+            return json.load(a)
 
     def get_timeframe_data(self):
         filename = os.path.join(self._dirname, self._timeframes_relpath)
         with open(filename) as tf:
-            timeframes = json.load(tf)
-        return timeframes
+            return json.load(tf)
 
 
 class Pandas:
     def __init__(self):
         self._dirname = os.path.dirname(__file__)
-    #     with open("info/assets.json") as assets:
-    #         self._assets = json.load(assets)
-    #     with open("info/timeframes.json") as timeframes:
-    #         self._timeframes = json.load(timeframes)
 
     def csv_to_pandas(self, asset_type, symbol, timeframe):
-        print("flipping csv to pandas")
         csv_relpath = f"candles/{asset_type}/{symbol}_{timeframe}.csv"
         csv_filename = os.path.join(self._dirname, csv_relpath)
-        data = pd.read_csv(csv_filename)
-        print(data)
-        return data
+
+        return pd.read_csv(csv_filename)
 
 
 class Visuals:
-    def plot(self):
-        df = Pandas().csv_to_pandas(asset_type="cryptocurrency", symbol="BTC", timeframe="hour")
-        df.plot(x="unix", y="close")
-        # df.show()
-        # plt.plot(df.loc["unix"], df.loc["close"], data=df)
-        # plt.plot([1,2,3], [1,2,3])
+    def plot(self, asset_type, symbol, timeframe, transactions):
+        prices = Pandas().csv_to_pandas(
+            asset_type=asset_type, symbol=symbol, timeframe=timeframe
+        )
+
+        merged_df = pd.merge(left=prices, right=transactions, how="outer", on="unix")
+        plt.plot(merged_df.unix, merged_df.close)
+
+        transactions_with_prices = merged_df[merged_df["sig"].notna()]
+        for index, row in transactions_with_prices.iterrows():
+            plt.annotate(row.sig, (row.unix, row.close))
+
         plt.show(block=True)
