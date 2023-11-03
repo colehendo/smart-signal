f613c612a467b0c7e558050222707846a205f212
Cole Henderson <cole.hendo.tech@gmail.com>
2023-11-03 10:29:06 -0700

    Update deploy.sh
    removed reference to sensitive data

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,4 @@ echo ""
 aws s3 sync --delete ./dist/smart-signal s3://smart-signal --profile smart-signal-sls
 
 echo ""
-echo "Updating files on EC2..."
-echo ""
-ssh -i ./smart-signal-frontend.pem -tt ec2-user@3.226.158.116 "sudo aws s3 sync --delete s3://smart-signal/ /var/www/html/"
-
-echo ""
 echo "Site has been updated."
